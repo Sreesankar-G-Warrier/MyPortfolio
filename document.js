a735e92f1c929551568d8143e1432a6d00783331
Sreesankar G Warrier <sreesankargwarrier@gmail.com>
2020-11-30 15:16:40 +0530

    Updated

--- document.js
+++ document.js
@@ -1,8 +1,8 @@
-document.getElementById("about-header").style.color = "indigo";
+document.getElementById("about-header").style.color = "whitesmoke";
 document.getElementById("about-header").style.cursor = "pointer";
-document.getElementById("work-header").style.color = "indigo";
+document.getElementById("work-header").style.color = "whitesmoke";
 document.getElementById("work-header").style.cursor = "pointer";
-document.getElementById("do-header").style.color = "indigo";
+document.getElementById("do-header").style.color = "whitesmoke";
 document.getElementById("do-header").style.cursor = "pointer";
 
 
@@ -15,9 +15,15 @@ document.getElementById("about-header").addEventListener("click",() => {
     }else{
         console.log("Showing the Block About");
         document.getElementById("tempHide").style.opacity= "5";
+        document.getElementById("about-header").style.textDecoration ="underline";
+        document.getElementById("about-header").style.zoom ="140%";
         document.getElementById("tempHide1").style.display = "none";
         document.getElementById("tempHide2").style.display = "none";
         document.getElementById("welcome").style.display = "none";
+        document.getElementById("do-header").style.textDecoration ="none";
+        document.getElementById("work-header").style.textDecoration ="none";
+        document.getElementById("work-header").style.zoom ="100%";
+        document.getElementById("do-header").style.zoom ="100%";
         document.getElementById("tempHide").style.display= "block";
     }
 });
@@ -30,9 +36,15 @@ document.getElementById("do-header").addEventListener("click",() => {
     }else{
         console.log("Showing the Block");
         document.getElementById("tempHide1").style.opacity= "5";
+        document.getElementById("do-header").style.textDecoration ="underline";
+        document.getElementById("do-header").style.zoom ="140%";
         document.getElementById("tempHide").style.display = "none";
         document.getElementById("tempHide2").style.display = "none";
         document.getElementById("welcome").style.display = "none";
+        document.getElementById("about-header").style.textDecoration ="none";
+        document.getElementById("work-header").style.textDecoration ="none";
+        document.getElementById("about-header").style.zoom ="100%";
+        document.getElementById("work-header").style.zoom ="100%";
         document.getElementById("tempHide1").style.display= "block";
     }
 });
@@ -44,9 +56,15 @@ document.getElementById("work-header").addEventListener("click",() => {
     }else{
         console.log("Showing the Block");
         document.getElementById("tempHide2").style.opacity= "5";
+        document.getElementById("work-header").style.textDecoration ="underline";
+        document.getElementById("work-header").style.zoom ="140%";
         document.getElementById("tempHide").style.display = "none";
         document.getElementById("tempHide1").style.display = "none";
         document.getElementById("welcome").style.display = "none";
+        document.getElementById("about-header").style.textDecoration ="none";
+        document.getElementById("do-header").style.textDecoration ="none";
+        document.getElementById("about-header").style.zoom ="100%";
+        document.getElementById("do-header").style.zoom ="100%";
         document.getElementById("tempHide2").style.display= "block";
     }
 });
