af69a9b415945c6b970386b33043fa86147a2767
Sreesankar G Warrier <sreesankargwarrier@gmail.com>
2020-11-30 17:12:16 +0530

    Update document.js

--- document.js
+++ document.js
@@ -1,11 +1,6 @@
-document.getElementById("about-header").style.color = "whitesmoke";
 document.getElementById("about-header").style.cursor = "pointer";
-document.getElementById("work-header").style.color = "whitesmoke";
 document.getElementById("work-header").style.cursor = "pointer";
-document.getElementById("do-header").style.color = "whitesmoke";
 document.getElementById("do-header").style.cursor = "pointer";
-
-
 
 document.getElementById("about-header").addEventListener("click",() => {
     if(document.getElementById("tempHide").style.display == "block"){
@@ -15,15 +10,17 @@ document.getElementById("about-header").addEventListener("click",() => {
     }else{
         console.log("Showing the Block About");
         document.getElementById("tempHide").style.opacity= "5";
-        document.getElementById("about-header").style.textDecoration ="underline";
-        document.getElementById("about-header").style.zoom ="140%";
+        document.getElementById("about-header").style.backgroundColor="whitesmoke"
+        document.getElementById("about-header").style.color="#333"
         document.getElementById("tempHide1").style.display = "none";
         document.getElementById("tempHide2").style.display = "none";
         document.getElementById("welcome").style.display = "none";
         document.getElementById("do-header").style.textDecoration ="none";
         document.getElementById("work-header").style.textDecoration ="none";
-        document.getElementById("work-header").style.zoom ="100%";
-        document.getElementById("do-header").style.zoom ="100%";
+        document.getElementById("do-header").style.backgroundColor="#333"
+        document.getElementById("do-header").style.color="whitesmoke"
+        document.getElementById("work-header").style.backgroundColor="#333"
+        document.getElementById("work-header").style.color="whitesmoke"
         document.getElementById("tempHide").style.display= "block";
     }
 });
@@ -36,15 +33,17 @@ document.getElementById("do-header").addEventListener("click",() => {
     }else{
         console.log("Showing the Block");
         document.getElementById("tempHide1").style.opacity= "5";
-        document.getElementById("do-header").style.textDecoration ="underline";
-        document.getElementById("do-header").style.zoom ="140%";
+        document.getElementById("do-header").style.backgroundColor="whitesmoke"
+        document.getElementById("do-header").style.color="#333"
         document.getElementById("tempHide").style.display = "none";
         document.getElementById("tempHide2").style.display = "none";
         document.getElementById("welcome").style.display = "none";
         document.getElementById("about-header").style.textDecoration ="none";
         document.getElementById("work-header").style.textDecoration ="none";
-        document.getElementById("about-header").style.zoom ="100%";
-        document.getElementById("work-header").style.zoom ="100%";
+        document.getElementById("about-header").style.backgroundColor="#333"
+        document.getElementById("about-header").style.color="whitesmoke"
+        document.getElementById("work-header").style.backgroundColor="#333"
+        document.getElementById("work-header").style.color="whitesmoke"
         document.getElementById("tempHide1").style.display= "block";
     }
 });
@@ -56,15 +55,17 @@ document.getElementById("work-header").addEventListener("click",() => {
     }else{
         console.log("Showing the Block");
         document.getElementById("tempHide2").style.opacity= "5";
-        document.getElementById("work-header").style.textDecoration ="underline";
-        document.getElementById("work-header").style.zoom ="140%";
+        document.getElementById("work-header").style.backgroundColor="whitesmoke"
+        document.getElementById("work-header").style.color="#333"
         document.getElementById("tempHide").style.display = "none";
         document.getElementById("tempHide1").style.display = "none";
         document.getElementById("welcome").style.display = "none";
         document.getElementById("about-header").style.textDecoration ="none";
         document.getElementById("do-header").style.textDecoration ="none";
-        document.getElementById("about-header").style.zoom ="100%";
-        document.getElementById("do-header").style.zoom ="100%";
+        document.getElementById("do-header").style.backgroundColor="#333"
+        document.getElementById("do-header").style.color="whitesmoke"
+        document.getElementById("about-header").style.backgroundColor="#333"
+        document.getElementById("about-header").style.color="whitesmoke"
         document.getElementById("tempHide2").style.display= "block";
     }
 });
